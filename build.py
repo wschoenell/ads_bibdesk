4657ae627a3863f8655f182287350dc0bc1b1710
Natalia Vale Asari <natalia.vale.asari@gmail.com>
2017-09-07 17:55:46 -0300

    Changed “Add to Bibdesk.workflow” to use the installed adsbibdesk script instead of hosting a copy inside the workflow.

--- build.py
+++ build.py
@@ -24,30 +24,12 @@ from xml.etree import ElementTree
 
 def main():
     rel_path = lambda path: os.path.join(os.path.dirname(__file__), path)
-    servicePath = rel_path("build/Add to BibDesk.workflow/Contents/document.wflow")
-    appPath = rel_path("build/ADS to BibDesk.app/Contents/document.wflow")
     builtPyPath = rel_path("build/adsbibdesk/adsbibdesk.py")
     origPyPath = rel_path("adsbibdesk.py")
 
     # Executable as a CLI interface for ADS to BibDesk unto its own
     shutil.copy(origPyPath, builtPyPath)
     os.chmod(builtPyPath, 0755)
-
-    for workflow in (servicePath, appPath):
-        xml = ElementTree.fromstring(open(workflow).read())
-        for arr in xml.find('dict').find('array').getchildren():
-
-            # fetch Python code inside the xml
-            py = [c for c in arr.find('dict').getchildren()
-                 if c.tag == 'dict' and
-                 any([i.text and '/usr/bin/env' in i.text for i in c.getchildren()])]
-
-            # rewrite with current file
-            if py:
-                py[0].find('string').text = open(builtPyPath).read()
-
-        open(workflow, 'w').write(ElementTree.tostring(xml))
-
 
 if __name__ == '__main__':
     main()
